2fadee36a5cba907bf851bed072296d7adae77c9
mixun <dmixun@gmail.com>
2015-07-31 17:50:58 +0800

    1

--- 未命名文件夹/startup.sh
+++ 未命名文件夹/startup.sh
@@ -4,7 +4,8 @@
 #
 # $Id$
 # -----------------------------------------------------------------------------
-
+#========
+==========#
 # Make sure prerequisite environment variables are set
 if [ -z "$JAVA_HOME" ]; then
   echo "The JAVA_HOME environment variable is not defined"
